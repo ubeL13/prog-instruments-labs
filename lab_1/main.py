dbd177e0ea4509a40342572d12e616dcb63a14a1
UbeL13 <u.b.2004@yandex.ru>
2024-11-22 02:02:56 +0400

    processing the length of strings to pep 8

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,11 +1,10 @@
 from datetime import date
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.exceptions import MessageNotModified
 
 from buttons import *
 from config import *
-from utils import profile_text, ref_text, pay_text, get_all_mamonts, add_mamont
+from utils import (profile_text, ref_text, pay_text, get_all_mamonts, add_mamont)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -49,7 +48,8 @@ async def worker_ref(call: types.CallbackQuery):
 @dp.callback_query_handler(lambda call: call.data == 'worker_buy')
 async def worker_buy(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
-    await bot.send_message(text=worker_buy_text, chat_id=call.message.chat.id)
+    await bot.send_message(text=worker_buy_text,
+                           chat_id=call.message.chat.id)
 
 
 @dp.message_handler(content_types=['text'])
@@ -57,7 +57,9 @@ async def worker_add_orders(message: types.Message):
     if 'накрутить' in message.text.lower():
         try:
             count = message.text.split(' ')[1]
-            await bot.send_message(text=profile_text(message.from_user.id, count), chat_id=message.chat.id)
+            await bot.send_message(text=profile_text(message.from_user.id,
+                                                      count),
+                                   chat_id=message.chat.id)
         except IndexError:
             pass
 
@@ -69,7 +71,8 @@ async def send_help(message: types.Message):
         "/start - Начать взаимодействие с ботом\n"
         "/воркер - Получить информацию о воркере\n"
         "/help - Показать это сообщение\n"
-        "Нажмите на кнопки в меню, чтобы получить доступ к другим функциям."
+        "Нажмите на кнопки в меню, чтобы получить доступ к другим "
+        "функциям."
     )
     await message.answer(help_text, reply_markup=markup_main)
 
@@ -88,7 +91,9 @@ async def main_layout(call: types.CallbackQuery):
 async def links(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     if int(call.data[-1]) == 0:
-        await bot.send_message(chat_id=call.message.chat.id, text=links_text, reply_markup=markup_feedback)
+        await bot.send_message(chat_id=call.message.chat.id,
+                               text=links_text,
+                               reply_markup=markup_feedback)
     else:
         try:
             await bot.edit_message_text(links_text,
@@ -117,7 +122,9 @@ async def feedback(call: types.CallbackQuery):
 async def buy_process(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     if int(call.data[-1]) == 0:
-        await bot.send_message(chat_id=call.message.chat.id, text=buy_text, reply_markup=markup_city)
+        await bot.send_message(chat_id=call.message.chat.id,
+                               text=buy_text,
+                               reply_markup=markup_city)
     else:
         await bot.edit_message_text(buy_text,
                                     reply_markup=markup_city,
@@ -130,7 +137,9 @@ async def buy_process(call: types.CallbackQuery):
 async def vacancies(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     if int(call.data[-1]) == 0:
-        await bot.send_message(chat_id=call.message.chat.id, text=vacancies_text, reply_markup=markup_main)
+        await bot.send_message(chat_id=call.message.chat.id,
+                               text=vacancies_text,
+                               reply_markup=markup_main)
     else:
         try:
             await bot.edit_message_text(vacancies_text,
@@ -146,7 +155,9 @@ async def vacancies(call: types.CallbackQuery):
 async def promo(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     if int(call.data[-1]) == 0:
-        await bot.send_message(chat_id=call.message.chat.id, text=ref_text(call.from_user.id), reply_markup=markup_main)
+        await bot.send_message(chat_id=call.message.chat.id,
+                               text=ref_text(call.from_user.id),
+                               reply_markup=markup_main)
     else:
         try:
             await bot.edit_message_text(ref_text(call.from_user.id),
@@ -162,7 +173,9 @@ async def promo(call: types.CallbackQuery):
 async def important(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     if int(call.data[-1]) == 0:
-        await bot.send_message(chat_id=call.message.chat.id, text=important_text, reply_markup=markup_main)
+        await bot.send_message(chat_id=call.message.chat.id,
+                               text=important_text,
+                               reply_markup=markup_main)
     else:
         try:
             await bot.edit_message_text(important_text,
@@ -178,7 +191,8 @@ async def important(call: types.CallbackQuery):
 async def profile(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     if int(call.data[-1]) == 0:
-        await bot.send_message(chat_id=call.message.chat.id, text=profile_text(call.from_user.id),
+        await bot.send_message(chat_id=call.message.chat.id,
+                               text=profile_text(call.from_user.id),
                                reply_markup=markup_main)
     else:
         try:
@@ -234,63 +248,81 @@ async def price_list(call: types.CallbackQuery):
 @dp.callback_query_handler(lambda call: call.data == 'price_back')
 async def price_list1(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
-    await bot.send_message(chat_id=call.message.chat.id, text=price_list_text, reply_markup=markup_price)
+    await bot.send_message(chat_id=call.message.chat.id,
+                           text=price_list_text,
+                           reply_markup=markup_price)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's1')
 async def s1(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s1.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s1_text, reply_markup=markup_s1)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s1_text,
+                         reply_markup=markup_s1)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's2')
 async def s2(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s2.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s2_text, reply_markup=markup_s2)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s2_text,
+                         reply_markup=markup_s2)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's3')
 async def s3(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s3.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s3_text, reply_markup=markup_s3)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s3_text,
+                         reply_markup=markup_s3)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's4')
 async def s4(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s4.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s4_text, reply_markup=markup_s4)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s4_text,
+                         reply_markup=markup_s4)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's5')
 async def s5(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s5.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s5_text, reply_markup=markup_s5)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s5_text,
+                         reply_markup=markup_s5)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's6')
 async def s6(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s6.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s6_text, reply_markup=markup_s6)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s6_text,
+                         reply_markup=markup_s6)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's7')
 async def s7(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s7.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s7_text, reply_markup=markup_s7)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s7_text,
+                         reply_markup=markup_s7)
 
 
 @dp.callback_query_handler(lambda call: call.data == 's8')
 async def s8(call: types.CallbackQuery):
     await bot.answer_callback_query(call.id)
     pic = open("images/s8.jpg", "rb")
-    await bot.send_photo(chat_id=call.message.chat.id, photo=pic, caption=s8_text, reply_markup=markup_s8)
+    await bot.send_photo(chat_id=call.message.chat.id,
+                         photo=pic, caption=s8_text,
+                         reply_markup=markup_s8)
 
 
 @dp.callback_query_handler(lambda call: 'pay' in call.data)
@@ -299,7 +331,3 @@ async def pay(call: types.CallbackQuery):
     await bot.send_message(chat_id=call.message.chat.id,
                            text=pay_text(prices[call.data[-2]][int(call.data[-1])]),
                            reply_markup=markup_pay)
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
