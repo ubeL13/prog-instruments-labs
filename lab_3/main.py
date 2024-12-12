98a3af57d1d7601824329fa4dc86dc0affccd4ab
UbeL13 <u.b.2004@yandex.ru>
2024-12-12 16:42:31 +0400

    correct work

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,29 +1,15 @@
 import csv
 import re
-import json
-from typing import List
+from typing import List, Optional
 from checksum import calculate_checksum, serialize_result
+import config
 
 
-def load_settings(file_path: str) -> dict:
-    """Загружает настройки из JSON файла.
-
-    Args:
-        file_path : Путь к конфигурационному файлу.
-
-    Returns:
-        dict: Словарь с настройками.
+def csv_to_list(file_path: str) -> Optional[List[List[str]]]:
     """
-    with open(file_path, "r") as file:
-        return json.load(file)
-
-
-def read_csv(file_path: str) -> List[List[str]]:
-    """Читает данные из CSV файла.
-
+    Читает данные из CSV файла.
     Args:
         file_path : Путь к файлу CSV.
-
     Returns:
         List : Список строк, каждая из которых представлена как список значений.
     """
@@ -31,42 +17,39 @@ def read_csv(file_path: str) -> List[List[str]]:
         return [row for row in csv.reader(file, delimiter=";")][1:]
 
 
-def is_valid(row: List[str], patterns: dict) -> bool:
-    """Проверяет, удовлетворяет ли строка регулярным выражениям.
-
+def is_valid(pattern: dict, row: List[str]) -> bool:
+    """
+    Проверяет, удовлетворяет ли строка регулярным выражениям.
     Args:
         row : Строка, представленная в виде списка значений.
         patterns : Словарь регулярных выражений.
-
     Returns:
         bool: True, если строка валидна, иначе False.
     """
-    return all(re.match(patterns[field], value) for field, value in zip(patterns.keys(), row))
+    return all(re.match(pattern[key], data) for key, data in zip(pattern.keys(), row))
 
 
-def find_invalid_indices(data: List[List[str]], patterns: dict) -> List[int]:
-    """Находит индексы строк, которые не соответствуют требованиям.
-
+def find_invalid_indices(pattern: dict, data: List[List[str]]) -> Optional[List[int]]:
+    """
+    Находит индексы строк, которые не соответствуют требованиям.
     Args:
         data : Список строк для проверки.
         patterns : Словарь регулярных выражений.
-
     Returns:
         List : Список индексов невалидных строк.
     """
-    return [index for index, row in enumerate(data) if not is_valid(row, patterns)]
+    return [i for i, row in enumerate(data) if not is_valid(pattern, row)]
 
 
 def main():
-    settings = load_settings("config.json")
+    var_name = config.var
+    input_csv = config.csv_file
+    output_json = config.json_file
+    regex_patterns = config.regulars
 
-    var_name = settings["VAR"]
-    input_csv = settings["CSV_FILE"]
-    output_json = settings["JSON_FILE"]
-    regex_patterns = settings["REGULARS"]
+    csv_data = csv_to_list(input_csv)
 
-    csv_data = read_csv(input_csv)
-    invalid_indices = find_invalid_indices(csv_data, regex_patterns)
+    invalid_indices = find_invalid_indices(regex_patterns, csv_data)
 
     checksum_result = calculate_checksum(invalid_indices)
     serialize_result(var_name, checksum_result, output_json)
